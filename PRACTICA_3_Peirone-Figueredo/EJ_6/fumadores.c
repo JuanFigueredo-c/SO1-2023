83cd766fa32cad895fe31266cefe50952121b584
Bauti Peirone <61645698+BatiDyDx@users.noreply.github.com>
2023-05-18 10:18:58 -0300

    EJ_6 updated with comments and solved bug

--- PRACTICA_3_Peirone-Figueredo/EJ_6/fumadores.c
+++ PRACTICA_3_Peirone-Figueredo/EJ_6/fumadores.c
@@ -4,7 +4,16 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t tabaco, papel, fosforos, otra_vez;
+/*
+ * La idea general de la implementacion es que un fumador espere para ver
+ * si sus recursos estan disponibles. Al entrar, usa un sem_trywait para
+ * ver si su primer recurso esta disponible. Si no lo esta, da lugar a que otro
+ * fumador pruebe. Si el primer recurso esta disponible, prueba con el segundo, y si
+ * lo esta puede fumar. Si su segundo recurso no esta disponible, devuelve el primero
+ * y nuevamente da lugar a que otro fumador intente. 
+*/
+
+sem_t tabaco, papel, fosforos, otra_vez, decidiendo;
 
 void agente() {
   while (1) {
@@ -23,36 +32,54 @@ void fumar(int fumador) {
 
 void* fumador1(void *arg) {
   while (1) {
-    sem_wait(&tabaco);
-    if (sem_trywait(&papel) != 0) {
-      sem_post(&tabaco);
+    sem_wait(&decidiendo); // Prueba si sus recursos estan disponibles
+    if (sem_trywait(&papel) != 0) { // Comprueba si su primer recurso esta disponible
+      sem_post(&decidiendo); // Deja que otro fumador pruebe
+      continue;
+    }
+    if (sem_trywait(&tabaco) != 0) { // Comprueba si su segundo recurso esta disponible
+      sem_post(&papel); // Devuelve el primer recurso
+      sem_post(&decidiendo); // Deja que otro fumador pruebe
       continue;
     }
     fumar(1);
+    sem_post(&decidiendo);
     sem_post(&otra_vez);
   }
 }
 
+// Analogo para los demas fumadores
 void* fumador2(void *arg) {
   while (1) {
-    sem_wait(&fosforos);
+    sem_wait(&decidiendo);
     if (sem_trywait(&tabaco) != 0) {
-      sem_post(&fosforos);
+      sem_post(&decidiendo);
+      continue;
+    }
+    if (sem_trywait(&fosforos) != 0) {
+      sem_post(&tabaco);
+      sem_post(&decidiendo);
       continue;
     }
     fumar(2);
+    sem_post(&decidiendo);
     sem_post(&otra_vez);
   }
 }
 
 void* fumador3(void *arg) {
   while (1) {
-    sem_wait(&papel);
+    sem_wait(&decidiendo);
     if (sem_trywait(&fosforos) != 0) {
-      sem_post(&papel);
+      sem_post(&decidiendo);
+    }
+    if (sem_trywait(&papel) != 0) {
+      sem_post(&fosforos);
+      sem_post(&decidiendo);
       continue;
     }
     fumar(3);
+    sem_post(&decidiendo);
     sem_post(&otra_vez);
   }
 }
@@ -63,10 +90,10 @@ int main() {
   sem_init(&papel, 0, 0);
   sem_init(&fosforos, 0, 0);
   sem_init(&otra_vez, 0, 1);
+  sem_init(&decidiendo, 0, 1);
   pthread_create(&s1, NULL, fumador1, NULL);
   pthread_create(&s2, NULL, fumador2, NULL);
   pthread_create(&s3, NULL, fumador3, NULL);
   agente();
   return 0;
 }
-
